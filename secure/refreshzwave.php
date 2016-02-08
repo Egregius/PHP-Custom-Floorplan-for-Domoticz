6fcb98283c5c78fcec243ca27232eb60bd6cffce
Guy Verschuere <guy@egregius.be>
2016-02-08 23:03:51 +0100

    20150208
    Update refreshzwave.php to reflect changes in OZWCP

--- secure/refreshzwave.php
+++ secure/refreshzwave.php
@@ -14,11 +14,12 @@ function RefreshZwave2($node) {
 		if($result=='OK') break;
 		sleep(1);
 	}
+	return $result;
 }
 function ZwaveHasnodefailed	($node) {
 	global $domoticzurl;
-	$zwaveurl=$domoticzurl.'ozwcp/refreshpost.html';
-	$zwavedata=array('fun'=>'hnf','node'=>$node);
+	$zwaveurl=$domoticzurl.'ozwcp/admpost.html';
+	$zwavedata=array('fun'=>'hnf','node'=>'node'.$node);
 	$zwaveoptions = array('http'=>array('header'=>'Content-Type: application/x-www-form-urlencoded\r\n','method'=>'POST','content'=>http_build_query($zwavedata),),);
 	$zwavecontext=stream_context_create($zwaveoptions);
 	for ($k=1;$k<=5;$k++){
@@ -27,19 +28,15 @@ function ZwaveHasnodefailed	($node) {
 		if($result=='OK') break;
 		sleep(1);
 	}
+	return $result;
 }
 function Zwavecancelaction() {
 	global $domoticzurl;
-	$zwaveurl=$domoticzurl.'ozwcp/refreshpost.html';
+	$zwaveurl=$domoticzurl.'ozwcp/admpost.html';
 	$zwavedata=array('fun'=>'cancel');
 	$zwaveoptions = array('http'=>array('header'=>'Content-Type: application/x-www-form-urlencoded\r\n','method'=>'POST','content'=>http_build_query($zwavedata),),);
 	$zwavecontext=stream_context_create($zwaveoptions);
-	for ($k=1;$k<=5;$k++){
-		sleep(1);
-		$result=file_get_contents($zwaveurl,false,$zwavecontext);
-		if($result=='OK') break;
-		sleep(1);
-	}
+	$result=file_get_contents($zwaveurl,false,$zwavecontext);
 }
 if(!empty($argv[1])&&!empty($argv[2])) {
 	RefreshZwave2($argv[1],$argv[2]);
@@ -49,14 +46,13 @@ else {
 	logwrite("RefreshZwave: no idx or name defined");
 }
 sleep(3);
-if($mc->get('deadnodes')<$tienmin) {
+if($mc->get('deadnodes')<$eenmin) {
 	foreach($devices as $node=>$data) {
 		if ($node == "result") {
 		foreach($data as $index=>$eltsNode) {
 		  if ($eltsNode["State"] == "Dead") {
-			telegram('Node '.$eltsNode['NodeID'].' '.$eltsNode['Description'].' ('.$eltsNode['Name'].') marked as dead, reviving.');
-			ZwaveHasnodefailed($eltsNode['NodeID']);
-			sleep(10);
+			telegram('Node '.$eltsNode['NodeID'].' '.$eltsNode['Description'].' ('.$eltsNode['Name'].') marked as dead, reviving '.ZwaveHasnodefailed($eltsNode['NodeID']));
+			sleep(2);
 			Zwavecancelaction();
 			//shell_exec('/var/www/secure/reboot.sh');
 		  }
