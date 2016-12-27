6ea0d30a16540dd74b40dba6b5af7d324d03cb4e
Guy Verschuere <guy@egregius.be>
2016-12-27 17:39:18 +0100

    Delete afterupdatedomoticz.sh

--- secure/afterupdatedomoticz.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find /volume1/@appstore/domoticz/www/ -type f -name "*.html" -o "*.js" -exec sudo sed -ir 's/\"iDisplayLength\"\s*:\s*[0-9]+,/\"iDisplayLength\" : -1, \"paging\": false,/g' {} +
